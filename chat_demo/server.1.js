0763ff21dcca85153453363bba005a91dc6ea89b
Raymond Camden <raymondcamden@gmail.com>
2024-04-30 17:10:57 -0500

    new demo

---
+++ chat_demo/server.1.js
@@ -0,0 +1,58 @@
+import * as http from 'http';
+import fs from 'fs'; 
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from '@google/generative-ai';
+
+const MODEL_NAME = "gemini-pro";
+const API_KEY = process.env.GOOGLE_AI_KEY;
+
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+let chat = null;
+
+async function callGemini(text) {
+
+	if(!chat) {
+		chat = model.startChat({
+			history:[]
+		});
+	}
+
+	const result = await chat.sendMessage(text);
+	console.log(JSON.stringify(result, null, '\t'));
+	return result.response.text();
+	
+}
+
+async function handler(req, res) {
+	console.log('Entered handler.', req.method, req.url);
+
+	if(req.method === 'GET' && req.url.indexOf('favicon.ico') === -1) {
+		res.writeHead(200, { 'Content-Type':'text/html' });
+		res.write(fs.readFileSync('./demo.html'));
+		res.end();
+	} else if(req.method === 'POST' && req.url === '/chat') {
+
+		let body = '';
+		req.on('data', chunk => {
+			body += chunk.toString();
+		});
+
+		req.on('end', async () => {
+			console.log('BODY:\n', JSON.stringify(body, null, '\t'));
+
+			let result = await callGemini(body);
+			console.log('result', result);
+			res.writeHead(200, { 'Content-Type':'text/plain' });
+			res.write(result);
+			res.end();
+
+		});
+
+	}
+
+}
+
+const server = http.createServer(handler);
+server.listen(3000);
+console.log('Listening on port 3000');
